94438372e05fa21e0d0d7a918d7d5760211bd389
Rajeevverma20 <rv208469@gmail.com>
2023-10-25 09:34:35 +0530

    Learning Day -2

---
+++ README.md
@@ -0,0 +1,21 @@
+# React Learn
+
+
+...
+# Parcel
+- Dev Build
+- Local server
+- HMR = Hot Module Replacement(Reload)
+- Filw Watching Alorigthm - written in C++
+- caching - Faster builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Code Splitting
+- Differential Bundling - support older browsers
+- Error Handling
+- Host app on HTTPs
+- Diagnostic (Error)
+- Tree Shaking -- remove unused code
+- Differnet dev and prod bundles
--- app.js
+++ app.js
@@ -1,20 +1,67 @@
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-    [  React.createElement("div", { id: "child" },[
-        [
-       React.createElement("h1", {}, "hii i am h1 tag"),
-       React.createElement("h2", {}, "hii i am h2 tag"),
-     ]
-     ]),
-     React.createElement("div", { id: "child2" },[
-        [
-       React.createElement("h1", {}, "hii i am h1 tag"),
-       React.createElement("h2", {}, "hii i am h2 tag"),
-     ]
-     ])]
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+//React.createElement =>ReactElement-JS Object  ==> HTML Element( Render)
+
+//React.createElement convert into object and render as a HTML element
+
+const heading = React.createElement("h1", { id: "hi" }, "Hello world");
+
+//JSX
+
+//JSX==> is not a HTML in JS. It is looklike HTML syntax
+
+//JSX Code (transpiled before it reaches the JS Engine) --transpiled this code by parcel- Babel
+ 
+//How is this code running --> JSX---> Babel transpiles it to React.createElement --->React.createElement =>ReactElement-JS Object  ==> HTML Element( Render)
+
+// const jsxHeading = (
+//     <h1 id="heading">Hello world from JSX </h1>
+// ) --> use for multi line circle brackets
+
+//React Element
+const jsxHeading = <h1 id="heading">Hello world from JSX </h1>;
+
+
+//React Component
+//Class Based Components -->(Old Way)
+//Functional  Based Components --->(New Way)
+
+//React Functional Components
+
+/*
+const HeadingComponent = () =>{
+    return <h1>Hello world functional Component</h1>
+}
+*/
+
+const number= 1000;
+
+//Short 
+const HeadingComponent = () => (
+<div id="container">
+   <h2> {number} </h2>
+<h1 className="heading">Hello world functional Component</h1>
+</div>
+)
+
+
+/*    Component Compositions
+const Title = () =>(
+    <h1 className="head"> Hello world using JSX</h1>
 );
 
-console.log(parent)
+const HeadingComponent = () =>(
+    <div id="container">
+    <Tittle />
+    <h1 className="heading">Hello world from functional  Components </h1>
+    </div>
+)
+
+*/
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+root.render(< HeadingComponent />);
+
+
+
