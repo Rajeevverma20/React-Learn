622b9b9aed2c09d81ce9fa352c2d1adf937237cc
Rajeevverma20 <rv208469@gmail.com>
2023-11-13 14:26:40 +0530

    Learning Day -6 Classy Component

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,7 +1,12 @@
+import User from "./uSER.JS";
+import UserClass from "./UserClass";
+
 const About = () =>{
     return (
         <div>
             <h1>About page</h1>
+            <User />
+            < UserClass name={"Rajeev Class"} />
         </div>
     )
 }
---
+++ src/Components/UserClass.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+class UserClass extends React.Component {
+
+    constructor(props){
+        super(props)
+    
+        this.state ={
+            userInfo:{
+                name: "Dummy",
+                location: "Dummy"
+            }
+        }
+    }
+
+    async componentDidMount(){
+        const data = await fetch("https://api.github.com/users/Rajeevverma20");
+
+        const json = await data.json();
+
+        this.setState({
+            userInfo:json,
+        })
+        console.log(json)
+    }
+
+    render () {
+
+        const { count } = this.state;
+        const {name} = this.state.userInfo.name;
+        return (
+            <div className="user-card">
+                <h2>Name :{name}</h2>
+                <h3>Location: Delhi</h3>
+                <h4>Contact: @Rajeecv</h4>
+            </div>
+        )
+    }
+
+}
+
+export default UserClass;
